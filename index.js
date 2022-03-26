2e85504142f03a9e606ec8b447a566758e2d8ea7
Desh Bagley <stemtrainer@gmail.com>
2022-03-25 21:53:30 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const saturdayFun = function(activity = 'roller-skate') {
+	return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office'){
+	return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair ='*'){
+
+	const innerFunction = function(param ='special'){
+		return `You are ${visualFlair}${param}${visualFlair}!`
+	}
+	return innerFunction
+}
